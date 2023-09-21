fcd1bfd43fb7d221dde87be8def8f4af1f77d25c
João Pedro Soares <joao.p.soares93@gmail.com>
2023-09-21 23:26:19 +0100

    Apple Frameworks App Setup

---
+++ Apple-Frameworks/Apple-Frameworks/FrameworkGridView.swift
@@ -0,0 +1,20 @@
+//
+//  FrameworkGridView.swift
+//  Apple-Frameworks
+//
+//  Created by João Pedro Soares on 21/09/2023.
+//
+
+import SwiftUI
+
+struct FrameworkGridView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FrameworkGridView_Previews: PreviewProvider {
+    static var previews: some View {
+        FrameworkGridView()
+    }
+}
