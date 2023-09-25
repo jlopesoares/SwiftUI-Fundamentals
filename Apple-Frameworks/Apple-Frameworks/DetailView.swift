409e5c5ac73ee8615059926fb5d1952314b890a6
João Pedro Soares <joao.p.soares93@gmail.com>
2023-09-25 23:14:02 +0100

    Add Detail View

--- Apple-Frameworks/Apple-Frameworks/DetailView.swift
+++ Apple-Frameworks/Apple-Frameworks/DetailView.swift
@@ -8,15 +8,47 @@
 import SwiftUI
 
 struct DetailView: View {
+    
+    var framework: Framework
+    
     var body: some View {
         VStack {
-            FrameworkTitleView(framwork: <#T##Framework#>)
+            
+            HStack{
+                Spacer()
+                
+                Button {
+                    
+                } label: {
+                    Image(systemName: "xmark")
+                        .foregroundColor(Color(.label))
+                        .imageScale(.large)
+                        .frame(width: 24, height: 24)
+                }
+            }
+            .padding()
+            
+            Spacer()
+            FrameworkTitleView(framework: framework)
+            
+            Text(framework.description)
+                .font(.body)
+                .padding()
+            
+            Spacer()
+            
+            Button {
+                
+            } label: {
+                AFButton(title: "Learn More")
+            }
         }
     }
 }
 
 struct DetailView_Previews: PreviewProvider {
     static var previews: some View {
-        DetailView()
+        DetailView(framework: MockData.sampleFramework)
+            .preferredColorScheme(.dark)
     }
 }
