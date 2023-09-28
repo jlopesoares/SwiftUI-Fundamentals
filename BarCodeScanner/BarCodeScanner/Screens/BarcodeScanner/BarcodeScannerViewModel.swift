0ed0ff427c191010bc32cfb46feb213b93b52d0f
João Pedro Soares <joao.p.soares93@gmail.com>
2023-09-28 22:49:18 +0100

    BarCodeScanner MVVM

---
+++ BarCodeScanner/BarCodeScanner/Screens/BarcodeScanner/BarcodeScannerViewModel.swift
@@ -0,0 +1,23 @@
+//
+//  BarcodeScannerViewModel.swift
+//  BarCodeScanner
+//
+//  Created by João Pedro on 28/09/2023.
+//
+
+import Foundation
+import SwiftUI
+
+final class BarcodeScannerViewModel: ObservableObject {
+    
+    @Published var scannedCode: String = ""
+    @Published var alertItem: AlertItem?
+    
+    var statusText: String {
+        scannedCode.isEmpty ? "Not Yet Scanned" : scannedCode
+    }
+    
+    var statusTextColor: Color {
+        scannedCode.isEmpty ? .red : .green
+    }
+}
