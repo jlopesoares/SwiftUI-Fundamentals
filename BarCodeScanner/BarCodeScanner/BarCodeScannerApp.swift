6ce28fefa73fe2508eea5c91e460e587aa8fd7f5
João Pedro Soares <joao.p.soares93@gmail.com>
2023-09-28 22:14:57 +0100

    Add BarcodeScanner app

---
+++ BarCodeScanner/BarCodeScanner/BarCodeScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarCodeScannerApp.swift
+//  BarCodeScanner
+//
+//  Created by João Pedro on 27/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct BarCodeScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            BarcodeScannerView()
+        }
+    }
+}
---
+++ BarCodeScanner/BarCodeScanner/ScannerView.swift
@@ -0,0 +1,47 @@
+//
+//  ScannerView.swift
+//  BarCodeScanner
+//
+//  Created by João Pedro on 27/09/2023.
+//
+
+import SwiftUI
+
+struct ScannerView: UIViewControllerRepresentable {
+
+    @Binding var scannedCode: String
+    
+    func makeUIViewController(context: Context) -> ScannerVC {
+        ScannerVC(scannerDelegate: context.coordinator)
+    }
+    
+    func updateUIViewController(_ uiViewController: ScannerVC, context: Context) {}
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(scannerView: self)
+    }
+
+    final class Coordinator: NSObject, ScannerVCDelegate {
+        
+        private let scannerView: ScannerView
+        
+        init(scannerView: ScannerView) {
+            self.scannerView = scannerView
+        }
+        
+        func didFind(barcode: String) {
+            print(barcode)
+            scannerView.scannedCode = barcode
+        }
+        
+        func didSurface(error: CameraError) {
+            print(error.rawValue)
+        }
+    }
+}
+
+struct ScannerView_Previews: PreviewProvider {
+    static var previews: some View {
+        ScannerView(scannedCode: .constant(""))
+    }
+}
