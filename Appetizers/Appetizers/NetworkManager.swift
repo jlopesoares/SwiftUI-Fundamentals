3625ea89e305ffd52b2f9189879f85b3ee1691ad
João Pedro Soares <joao.p.soares93@gmail.com>
2023-10-04 18:37:34 +0100

    Add image downloader  + cache

--- Appetizers/Appetizers/NetworkManager.swift
+++ Appetizers/Appetizers/NetworkManager.swift
@@ -5,11 +5,12 @@
 //  Created by João Pedro Soares on 03/10/2023.
 //
 
-import Foundation
+import UIKit
 
 final class NetworkManager {
     
     static let shared = NetworkManager()
+    private let cache = NSCache<NSString, UIImage>()
     
     static let baseURL = "https://seanallen-course-backend.herokuapp.com/swiftui-fundamentals"
     private let appetizersURL = baseURL + "/appetizers"
@@ -51,5 +52,32 @@ final class NetworkManager {
                 completed(.failure(.invalidData))
             }
         }.resume()
+    }
+    
+    func downloadImage(fromURLString urlString: String, completed: @escaping (UIImage?) -> ()) {
+        let cacheKey = NSString(string: urlString)
+        if let image = cache.object(forKey: cacheKey){
+            completed(image)
+            return
+        }
+        
+        guard let url = URL(string: urlString) else {
+            completed(nil)
+            return
+        }
+        
+        let task = URLSession.shared.dataTask(with: URLRequest(url: url)) { data, response, _ in
+            guard
+                let data = data,
+                let image = UIImage(data: data)
+            else {
+                completed(nil)
+                return
+            }
+            self.cache.setObject(image, forKey: cacheKey)
+            completed(image)
+        }
+        
+        task.resume()
     }
 }
