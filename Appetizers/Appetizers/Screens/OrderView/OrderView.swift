6bda7742487adba71109f64e9f206cde901a958c
João Pedro Soares <joao.p.soares93@gmail.com>
2023-10-16 23:35:03 +0100

    Added onDelete

--- Appetizers/Appetizers/Screens/OrderView/OrderView.swift
+++ Appetizers/Appetizers/Screens/OrderView/OrderView.swift
@@ -8,11 +8,34 @@
 import SwiftUI
 
 struct OrderView: View {
+    
+    @State private var orderItems = MockData.orderItems
+    
     var body: some View {
         NavigationView {
-            Text("Orders")
-                .navigationTitle("🧾 Orders")
+            VStack {
+                List{
+                    ForEach(orderItems) { appetizer in
+                        AppetizerListCell(appetizer: appetizer)
+                    }
+                    .onDelete(perform: deleteItems)
+                }
+                .listStyle(PlainListStyle())
+                
+                Button {
+                    print("Order place")
+                } label: {
+                    APButton(title: "$99.99 - Place Order")
+                }
+                .padding(.bottom, 24)
+
+            }
+            .navigationTitle("🧾 Orders")
         }
+    }
+    
+    func deleteItems(at offsets: IndexSet) {
+        orderItems.remove(atOffsets: offsets)
     }
 }
 
