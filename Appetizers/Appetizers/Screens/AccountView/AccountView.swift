48fcb2128d15722bf16f90d7974fe2124c2114b7
João Pedro Soares <joao.p.soares93@gmail.com>
2023-10-17 23:33:59 +0100

    Added FocusState

--- Appetizers/Appetizers/Screens/AccountView/AccountView.swift
+++ Appetizers/Appetizers/Screens/AccountView/AccountView.swift
@@ -10,17 +10,34 @@ import SwiftUI
 struct AccountView: View {
 
     @StateObject var viewModel = AccountViewModel()
+    @FocusState private var focusedTextfield: FormTextField?
+    
+    enum FormTextField {
+        case firstName, lastName, email
+    }
     
     var body: some View {
         NavigationView {
             Form {
                 Section(header: Text("Personal Info")) {
                     TextField("First Name", text: $viewModel.user.firstName)
+                        .focused($focusedTextfield, equals: .firstName)
+                        .onSubmit { focusedTextfield = .lastName }
+                        .submitLabel(.next)
+                    
                     TextField("Last Name", text: $viewModel.user.lastName)
+                        .focused($focusedTextfield, equals: .lastName)
+                        .onSubmit { focusedTextfield = .email }
+                        .submitLabel(.next)
+                    
                     TextField("Email", text: $viewModel.user.email)
                         .keyboardType(.emailAddress)
                         .autocapitalization(.none)
                         .disableAutocorrection(true)
+                        .focused($focusedTextfield, equals: .email)
+                        .onSubmit { focusedTextfield = nil }
+                        .submitLabel(.continue)
+                    
                     DatePicker("Birthdate",
                                selection: $viewModel.user.birthdate,
                                displayedComponents: .date)
@@ -39,6 +56,13 @@ struct AccountView: View {
                 .toggleStyle(SwitchToggleStyle(tint: .brandPrimary))
             }
             .navigationTitle("👨 Account")
+            .toolbar {
+                ToolbarItemGroup(placement: .keyboard, content: {
+                    Button("Dismiss") {
+                        focusedTextfield = nil
+                    }
+                })
+            }
         }
         .alert(item: $viewModel.alertItem) { alertItem in
             Alert(title: alertItem.title,
